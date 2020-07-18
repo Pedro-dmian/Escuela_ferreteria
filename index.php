de47903f088535b97c5669e2644ad5e2fe937dd8
Damian Palomo <damian@teknik.mx>
2020-07-18 17:41:11 -0500

    Table Dashboard

--- index.php
+++ index.php
@@ -3,9 +3,66 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link rel="stylesheet" href="./assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="./assets/lib/datatable/datatables.min.css">
+    <link rel="stylesheet" href="./assets/css/custom.css">
+
+    <title>Productos</title>
 </head>
 <body>
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem veritatis sed ea voluptatibus eos optio dicta officia ducimus quae aperiam laborum laudantium sint culpa voluptatum, repudiandae ratione animi minima nesciunt.
+    <h1 class="text-center mt-5">Productos</h1>
+    <div class="row d-flex justify-content-center">
+        <div class="col-md-6">
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th>
+                            ID
+                        </th>
+                        <th>
+                            SKU
+                        </th>
+                        <th>
+                            Nombre del producto
+                        </th>
+
+                        <th>
+                            Descripción
+                        </th>
+                        <th>
+                            Precio
+                        </th>
+                        <th class="text-center">
+                            ....
+                        </th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>1</td>
+                        <td>Lorem</td>
+                        <td>Producto prueba</td>
+                        <td>Lorem ipsum dolor</td>
+                        <td>$ 300</td>
+                        <td>
+                            <button type="button" class="btn" id="UpdatedProduct" title="Editar">
+                                ✏
+                            </button>
+                            <button type="button" class="btn" id="RemoveProduct" title="Eliminar">
+                                💩
+                            </button>
+                        </td>
+                    </tr>
+                </tbody>
+
+
+            </table>
+        </div>
+    </div>
+
+
+    <script src="./assets/js/jquery-3.5.1.slim.min.js"></script>
+    <script src="./assets/js/bootstrap.min.js"></script>
+    <script src="./assets/js/main.js"></script>
 </body>
 </html>
