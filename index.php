c1459fb07bff75f39014b07f6d1c71765413f9b5
Damian Palomo <damian@teknik.mx>
2020-07-31 19:42:51 -0500

    Diseño de la tabla

--- index.php
+++ index.php
@@ -10,56 +10,70 @@
     <title>Productos</title>
 </head>
 <body>
-    <h1 class="text-center mt-5">Productos</h1>
-    <div class="row d-flex justify-content-center">
-        <div class="col-md-6">
-            <table class="table">
-                <thead>
-                    <tr>
-                        <th>
-                            ID
-                        </th>
-                        <th>
-                            SKU
-                        </th>
-                        <th>
-                            Nombre del producto
-                        </th>
 
-                        <th>
-                            Descripción
-                        </th>
-                        <th>
-                            Precio
-                        </th>
-                        <th class="text-center">
-                            ....
-                        </th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td>1</td>
-                        <td>Lorem</td>
-                        <td>Producto prueba</td>
-                        <td>Lorem ipsum dolor</td>
-                        <td>$ 300</td>
-                        <td>
-                            <button type="button" class="btn" id="UpdatedProduct" title="Editar">
-                                ✏
-                            </button>
-                            <button type="button" class="btn" id="RemoveProduct" title="Eliminar">
-                                💩
-                            </button>
-                        </td>
-                    </tr>
-                </tbody>
+    <div class="container">
+        <div class="tt-portlet">
+            <div class="tt-portlet-header">
+                <div class="row">
+                    <div class="col-md-9">
+                        <p class="h6 tt-portlet-title">
+                            Lista de Productos
+                        </p>
+                    </div>
+                    <div class="col-md-3 d-flex justify-content-end">
+                        <button type="button" class="btn btn--style">Agregar Producto</button>
+                    </div>
+                </div>
+            </div>
+
+            <div class="tt-portlet-body">
+                <table class="table tt-table" id="tableProducts">
+                    <thead>
+                        <tr>
+                            <th>
+                                ID
+                            </th>
+                            <th>
+                                SKU
+                            </th>
+                            <th>
+                                Nombre del producto
+                            </th>
+
+                            <th>
+                                Descripción
+                            </th>
+                            <th>
+                                Precio
+                            </th>
+                            <th class="text-center">
+                                ....
+                            </th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>1</td>
+                            <td>Lorem</td>
+                            <td>Producto prueba</td>
+                            <td>Lorem ipsum dolor</td>
+                            <td>$ 300</td>
+                            <td class="text-center">
+                                <button type="button" class="btn" id="UpdatedProduct" title="Editar">
+                                    ✏
+                                </button>
+                                <button type="button" class="btn" id="RemoveProduct" title="Eliminar">
+                                    💩
+                                </button>
+                            </td>
+                        </tr>
+                    </tbody>
 
 
-            </table>
+                </table>
+            </div>
         </div>
     </div>
-
 
     <script src="./assets/js/jquery-3.5.1.slim.min.js"></script>
     <script src="./assets/js/bootstrap.min.js"></script>
